248f0d9d71ad75118ecefb9ef3430d2ff850b4a9
Angel Corredor <adcorredorm@unal.edu.co>
2020-11-26 16:39:49 -0500

    vertical barchart and hover

--- chart.js
+++ chart.js
@@ -1,50 +1,72 @@
 const svg = d3.select('svg');
 const height = parseFloat(svg.attr('height'));
 const width = parseFloat(svg.attr('width'));
-const year = '2010';
+const year = '2019';
 
 function render(data) {
-  const xValue = (d) => d[year];
-  const yValue = (d) => d['Country Name'];
-  const margin = { top: 20, right: 20, bottom: 20, left: 120 };
+  const xValue = (d) => d['Country Name'];
+  const yValue = (d) => d[year];
+  const margin = { top: 20, right: 20, bottom: 20, left: 30 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
   const xScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, xValue)])
-    .range([0, innerWidth]);
-
-  const yScale = d3
     .scaleBand()
-    .domain(data.map(yValue))
-    .range([0, innerHeight])
+    .domain(data.map(xValue))
+    .range([0, innerWidth])
     .padding(0.05);
+
+  const maxdata = d3.max(data, yValue);
+
+  const inverse = (d) => maxdata - d;
+
+  const yScale = d3.scaleLinear().domain([0, maxdata]).range([innerHeight, 0]);
 
   const g = svg
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-  g.append('g')
-    .call(d3.axisLeft(yScale))
-    .selectAll('.domain, .tick line')
-    .remove();
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickSize(innerWidth)
+    .tickFormat(d3.format('.2s'));
 
-  const xAxis = d3
-    .axisTop(xScale)
-    .tickFormat(d3.format('.2s'))
-    .tickSize(-innerHeight);
-
-  g.append('g').call(xAxis).selectAll('.domain').remove();
+  g.append('g').call(yAxis).attr('transform', `translate(${innerWidth}, 0)`);
+  g.selectAll('.domain').remove();
   g.selectAll('.tick line').attr('stroke', '#C0C088');
+
+  const xAxis = d3.axisBottom(xScale).tickSize(innerHeight);
+
+  g.append('g').call(xAxis).selectAll('.domain, .tick line').remove();
 
   g.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
+    .attr('x', (d) => xScale(xValue(d)))
     .attr('y', (d) => yScale(yValue(d)))
-    .attr('width', (d) => xScale(xValue(d)))
-    .attr('height', yScale.bandwidth());
+    .attr('height', (d) => yScale(0) - yScale(yValue(d)))
+    .attr('width', xScale.bandwidth())
+    .on('mouseover', mouseover)
+    .on('mouseout', mouseout);
+
+  function mouseover(e, d) {
+    d3.select(this).attr('fill', '#b30707');
+    console.log(
+      g
+        .append('text')
+        .attr('id', d['Country Code'])
+        .attr('x', xScale(xValue(d)))
+        .attr('y', yScale(yValue(d)) - 5)
+        .text(xValue(d) + '-' + yValue(d))
+        .attr('fill', '#b30707')
+    );
+  }
+
+  function mouseout(e, d) {
+    d3.select(this).attr('fill', 'black');
+    g.select('#' + d['Country Code']).remove();
+  }
 }
 
 function load(a, b) {
